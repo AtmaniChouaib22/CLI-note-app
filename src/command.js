f0a23e6e4f30efae544bff1865d41e0ab32c945c
chouaibAtm <atmanichouaib22@gmail.com>
2025-03-18 11:24:56 +0100

    Refactor command handling and update database path for improved functionality

--- src/command.js
+++ src/command.js
@@ -1,66 +1,105 @@
-import {hideBin} from 'yargs/helpers'
-import yargs from 'yargs'
-import {newNote, getAllNotes, findNotes,removeNote, removeAllNotes} from './notes.js'
-import { start } from './server.js'
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs";
+import {
+  newNote,
+  getAllNotes,
+  findNotes,
+  removeNote,
+  removeAllNotes,
+} from "./notes.js";
+import { start } from "./server.js";
 
-const listNotes = notes => {
-  notes.forEach(({id, content, tags}) => {
-    console.log("id:", id); 
-    console.log("tags: ", tags)
-    console.log("content: ", content); 
+const listNotes = (notes) => {
+  notes.forEach(({ id, content, tags }) => {
+    console.log("id:", id);
+    console.log("tags: ", tags);
+    console.log("content: ", content);
     console.log("\n");
   });
-}
+};
 
 yargs(hideBin(process.argv))
-  .command('new <content>', 'create a new note', yargs => {
-    return yargs.positional('content', 
-    {description: 'the content of the note you want to create',
-        type: 'string'
-    }) },async argv => {
-    const tags = argv.tags ? argv.tags.split(','): [] ; 
-    const note = await newNote(argv.content, tags) ; 
-    console.info("new note: ",note)
-  }).option('tags', {
-    alias: 't', // short option for --tags
-    type: 'string',
-    description: 'tags to add to the note'
-  }).command('all', 'get all notes', () => {}, async (argv) => {
-    const notes = await getAllNotes() ; 
-    listNotes(notes) ; 
+  .command(
+    "new <content>",
+    "create a new note",
+    (yargs) => {
+      return yargs.positional("content", {
+        description: "the content of the note you want to create",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const tags = argv.tags ? argv.tags.split(",") : [];
+      const note = await newNote(argv.content, tags);
+      console.info("new note: ", note);
+    }
+  )
+  .option("tags", {
+    alias: "t", // short option for --tags
+    type: "string",
+    description: "tags to add to the note",
   })
-  .command('find <filter>', 'get matching notes', yargs => {
-    return yargs.positional('filter', {
-      describe: 'The search term to filter notes by, will be applied to note.content',
-      type: 'string'
-    })
-  }, async (argv) => {
-    const notes = await findNotes(argv.filter) ; 
-    listNotes(notes)
-  })
-  .command('remove <id>', 'remove a note by id', yargs => {
-    return yargs.positional('id', {
-      type: 'number',
-      description: 'The id of the note you want to remove'
-    })
-  }, async (argv) => {
-    const id = await removeNote(argv.id)
-    console.log(id); 
-  })
-  .command('web [port]', 'launch website to see notes', yargs => {
-    return yargs
-      .positional('port', {
-        describe: 'port to bind on',
+  .command(
+    "all",
+    "get all notes",
+    () => {},
+    async (argv) => {
+      const notes = await getAllNotes();
+      listNotes(notes);
+    }
+  )
+  .command(
+    "find <filter>",
+    "get matching notes",
+    (yargs) => {
+      return yargs.positional("filter", {
+        describe:
+          "The search term to filter notes by, will be applied to note.content",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const notes = await findNotes(argv.filter);
+      listNotes(notes);
+    }
+  )
+  .command(
+    "remove <id>",
+    "remove a note by id",
+    (yargs) => {
+      return yargs.positional("id", {
+        type: "number",
+        description: "The id of the note you want to remove",
+      });
+    },
+    async (argv) => {
+      const id = await removeNote(argv.id);
+      console.log(id);
+    }
+  )
+  .command(
+    "web [port]",
+    "launch website to see notes",
+    (yargs) => {
+      return yargs.positional("port", {
+        describe: "port to bind on",
         default: 5000,
-        type: 'number'
-      })
-  }, async (argv) => {
-    const notes = await getAllNotes() ; 
-    start(notes, argv.port); 
-  })
-  .command('clean', 'remove all notes', () => {}, async (argv) => {
-    await removeAllNotes(); 
-    console.log("db reseted"); 
-  })
+        type: "number",
+      });
+    },
+    async (argv) => {
+      const notes = await getAllNotes();
+      start(notes, argv.port);
+    }
+  )
+  .command(
+    "clean",
+    "remove all notes",
+    () => {},
+    async (argv) => {
+      await removeAllNotes();
+      console.log("db reseted");
+    }
+  )
   .demandCommand(1)
-  .parse() 
+  .parse();
--- src/db.js
+++ src/db.js
@@ -1,19 +1,21 @@
-import fs from 'node:fs/promises'
+import fs from "node:fs/promises";
 
-const DB_PATH = "C:\\Users\\THINKPAD\\Desktop\\nodeLearn\\db.json"
+const DB_PATH = "/home/chouaib/repos/CLI-note-app/DB.json";
 
 export const getDB = async () => {
-    const db = await fs.readFile(DB_PATH, 'utf-8');
-    return JSON.parse(db); }
+  const db = await fs.readFile(DB_PATH, "utf-8");
+  return JSON.parse(db);
+};
 
 export const saveDB = async (db) => {
-    await fs.writeFile(DB_PATH, JSON.stringify(db,null,2))
-    return db ;
-}
+  await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
+  return db;
+};
 
-export const insertDB = async (note) => { 
-    const db = await getDB(); 
-    db.notes.push(note); 
-    await saveDB(db)
-    return note ; 
-}
+export const insertDB = async (note) => {
+  const db = await getDB();
+  console.log(db);
+  db.notes.push(note);
+  await saveDB(db);
+  return note;
+};
